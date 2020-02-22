86b3f373afde0d3fed603c21a29ebf66e60854db
Isabel Blomström <isabel.blomstrom@gmail.com>
2020-02-22 20:58:34 +0100

    Proof of concept

--- src/App.svelte
+++ src/App.svelte
@@ -1,25 +1,44 @@
 <script>
 	export let name;
-</script>
+	let count = 0;
+	function handleClick() {
+		count += 1;
+	}
+	</script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>Hej {name}!</h1>
+	<button on:click={handleClick}>
+	Clicked {count}
+	</button>
 </main>
 
 <style>
+
 	main {
-		text-align: center;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-direction: column;
 		padding: 1em;
 		max-width: 240px;
+		height: 100vh;
 		margin: 0 auto;
+		background-image: linear-gradient(to top, 
+		#cd06b5, #ff0081, #ff5f4a, 
+		#ffa40c, #ebde12);
 	}
 
 	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
+		color: black;
+		text-transform: uppcercase;
 		font-size: 4em;
 		font-weight: 100;
+	}
+
+	button {
+		border-radius: 2rem;
+		padding: 1rem;
 	}
 
 	@media (min-width: 640px) {
